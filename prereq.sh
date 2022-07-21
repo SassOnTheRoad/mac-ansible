d6569b4364bf1b4c3fa7ab24ef59f7412d674e5c
SassOnTheRoad <sasson.mansoori@gmail.com>
2022-07-21 08:48:34 +0100

    initial push

---
+++ prereq.sh
@@ -0,0 +1,33 @@
+# Xcode
+xcode-select --install
+
+# git repo
+# get git ssh key 
+git clone git@github.com:SassOnTheRoad/mac-ansible.git
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/sasson/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew analytics off
+
+# pyenv
+brew install pyenv
+pyenv install 3.10.5 # <latest_python_version> (Check playbook up to date)
+
+# Make sure pyenv's python on path (it will be after my shell settings are in place from playbook)
+/Users/sasson/.pyenv/versions/3.10.5/bin/python -m venv ansible_venv
+source ansible_venv/bin/activate
+
+pip install ansible
+python -m pip install --upgrade pip
+
+# Then 
+./playbooks/sass_mac.yml 
+
+
+deactivate
+
+
+
+
